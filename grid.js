9c61aaf9b7cb9712f311000e5a56a5a5cb50371d
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-29 16:52:04 +0100

    defining the rows and columns

--- grid.js
+++ grid.js
@@ -1,22 +1,15 @@
-function solution(n, m, figures){
-    function create2DArray(numRows, numCols) {
-  let arr = new Array(numRows); // Create an array for the rows
+function solution(n, m, figures) {
+    let arr = new Array(n); // Create an array for the rows
 
-  for (let i = 0; i < numRows; i++) {
-    arr[i] = new Array(numCols).fill(0); // Create an array for each row and fill with a default value (e.g., 0)
-  }
-  return arr;
+    for (let i = 0; i < n; i++) {
+        arr[i] = new Array(m).fill(0); // Create an array for each row and fill with a default value (e.g., 0)
+    }
+    return arr;
 }
 
-// Example usage:
-let myMatrix = create2DArray(3, 4); // Creates a 3x4 matrix filled with zeros
-console.log(myMatrix);
-/*
-Output:
-[
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0]
-]
-*/
-}
+
+    // Example usage:
+solution1 = solution(4, 4, [D, B, A, C]); // Creates a 3x4 matrix filled with zeros
+    
+
+// }
